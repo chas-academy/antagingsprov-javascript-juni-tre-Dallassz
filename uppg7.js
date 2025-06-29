13e7f5b60009b9baca8be371cb62f1907a914cd8
eric_dahlgren_@hotmail.com <eric_dahlgren_@hotmail.com>
2025-06-29 22:28:00 +0200

    uppg7

--- uppg7.js
+++ uppg7.js
@@ -5,6 +5,9 @@ function uppg7() {
     // skapa en funktion som tar in två tal som argument
     // funktionen ska returnera summan av talen
     // kalla på funktionen i en return statement och ge den parametrarna 5 och 10
+const sum = (num1, num2) => num1 + num2
+
+return sum (5, 10)
 
  
 }
