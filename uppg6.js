7f058453e5156cb54c115509d0b3381b3947c11f
eric_dahlgren_@hotmail.com <eric_dahlgren_@hotmail.com>
2025-06-29 16:50:13 +0200

    uppg6

--- uppg6.js
+++ uppg6.js
@@ -4,8 +4,11 @@ function uppg6(){
 
     // Skapa en array med namn som du döper till names
     // Loopa igenom arrayen och skriv ut alla namn i konsolen
+let listofNames = ["Fredrik", "Niklas", "Jakob"];
 
- 
+    for (let i = 0; i < listofNames.length; i++) {
+        console.log(listofNames[i]);
+    }
 }
 
 module.exports = { uppg6 };
