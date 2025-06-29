d600a174ca9ed72c12116240e8e26c21ee0dd2ad
eric_dahlgren_@hotmail.com <eric_dahlgren_@hotmail.com>
2025-06-29 23:28:29 +0200

    uppg9.0

--- uppg9.js
+++ uppg9.js
@@ -1,6 +1,4 @@
 
-
-function uppg9 (){
     // skapa en funktion som heter sort och tar in en array av siffror som argument
     // i funktionen du loopa igenom arrayen och kolla med en if-sats om  talet är jämt eller udda
     // i if-sattsen skriv ut i konsolen resultatet
@@ -8,7 +6,21 @@ function uppg9 (){
 
     // anropa funktionen och skicka med en array som argument
 
-  
+    function uppg9() {
+  const numbers = [2, 4, 7, 10];
+
+  function sort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] % 2 === 0) {
+        console.log(arr[i], "jämt");
+      } else {
+        console.log(arr[i], "udda");
+      }
+    }
+  }
+
+  sort(numbers);
 }
+
 
 module.exports = { uppg9 };
